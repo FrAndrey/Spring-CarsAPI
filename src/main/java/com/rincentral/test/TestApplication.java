61270320e8a8eb782d97ab05e1ad9bdeb742b57b
FrAndrey <aparamon@my.centennialcollege.ca>
2021-02-23 13:06:14 +0700

    initial commit by Andrey

--- src/main/java/com/rincentral/test/TestApplication.java
+++ src/main/java/com/rincentral/test/TestApplication.java
@@ -1,7 +1,9 @@
 package com.rincentral.test;
+//by Andrey Fr on 2021
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class TestApplication {
