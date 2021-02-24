59050b38486aa222fef0f45018a646f82a878d7b
FrAndrey <aparamon@my.centennialcollege.ca>
2021-02-24 14:44:45 +0700

    BodyCharacteristics class implemented with props, getters and setters

--- src/main/java/com/rincentral/test/models/BodyCharacteristics.java
+++ src/main/java/com/rincentral/test/models/BodyCharacteristics.java
@@ -1,5 +1,48 @@
 package com.rincentral.test.models;
 
 public class BodyCharacteristics {
-    
+
+    private Integer bodyLength;
+    private Integer bodyWidth;
+    private Integer bodyHeight;
+    private String bodyStyle;
+
+    public BodyCharacteristics(Integer bodyLength, Integer bodyWidth, Integer bodyHeight, String bodyStyle) {
+        this.bodyLength = bodyLength;
+        this.bodyWidth = bodyWidth;
+        this.bodyHeight = bodyHeight;
+        this.bodyStyle = bodyStyle;
+    }
+
+    public Integer getBodyLength() {
+        return bodyLength;
+    }
+
+    public void setBodyLength(Integer bodyLength) {
+        this.bodyLength = bodyLength;
+    }
+
+    public Integer getBodyWidth() {
+        return bodyWidth;
+    }
+
+    public void setBodyWidth(Integer bodyWidth) {
+        this.bodyWidth = bodyWidth;
+    }
+
+    public Integer getBodyHeight() {
+        return bodyHeight;
+    }
+
+    public void setBodyHeight(Integer bodyHeight) {
+        this.bodyHeight = bodyHeight;
+    }
+
+    public String getBodyStyle() {
+        return bodyStyle;
+    }
+
+    public void setBodyStyle(String bodyStyle) {
+        this.bodyStyle = bodyStyle;
+    }
 }
