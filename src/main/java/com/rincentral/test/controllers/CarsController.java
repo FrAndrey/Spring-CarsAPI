7fba9ccf889ad7745c9e54dcb3f513369d48301a
FrAndrey <aparamon@my.centennialcollege.ca>
2021-02-26 17:55:14 +0700

    Simple greeting on the home page

--- src/main/java/com/rincentral/test/controllers/CarsController.java
+++ src/main/java/com/rincentral/test/controllers/CarsController.java
@@ -1,9 +1,12 @@
 package com.rincentral.test.controllers;
 
+
 import com.rincentral.test.models.external.ExternalCar;
 import com.rincentral.test.services.ExternalCarsApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -18,7 +21,16 @@ public class CarsController {
         return apiService.loadAllCars();
     }
 
+    @GetMapping("/")
+    @ResponseBody
+    public String greeting(){
+        return "Welcome to the Test task written by Andrey <a href = \"http://t.me/frandrey\">Telegram</a>";
+    }
 
+//    @GetMapping("/custom")
+//    public Iterable<CarFullInfo> list() {
+//        return carService.list();
+//    }
 
 
 }
