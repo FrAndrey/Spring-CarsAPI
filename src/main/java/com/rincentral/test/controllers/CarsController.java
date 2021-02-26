43085cf710b126a2a3d21dc24569526e15e433d6
FrAndrey <aparamon@my.centennialcollege.ca>
2021-02-26 23:56:14 +0700

    moved all the get methods to API

--- src/main/java/com/rincentral/test/controllers/CarsController.java
+++ src/main/java/com/rincentral/test/controllers/CarsController.java
@@ -1,36 +1,28 @@
 package com.rincentral.test.controllers;
 
 
+import com.rincentral.test.models.CarCollection;
+import com.rincentral.test.models.CarFullInfo;
+import com.rincentral.test.models.CarInfo;
 import com.rincentral.test.models.external.ExternalCar;
 import com.rincentral.test.services.ExternalCarsApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class CarsController {
-
-    @Autowired
-    private ExternalCarsApiService apiService;
-
-    @RequestMapping("/cars")
-    public List<ExternalCar> getAllCars() {
-        return apiService.loadAllCars();
-    }
 
     @GetMapping("/")
     @ResponseBody
     public String greeting(){
         return "Welcome to the Test task written by Andrey <a href = \"http://t.me/frandrey\">Telegram</a>";
     }
-
-//    @GetMapping("/custom")
-//    public Iterable<CarFullInfo> list() {
-//        return carService.list();
-//    }
 
 
 }
